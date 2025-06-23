6a5cb5e0883030120cf993d9b1591b97bba64daf
Symmetric-Web <symmetricwebsite@gmail.com>
2025-06-23 13:35:08 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -48,14 +48,5 @@ function App() {
     </Router>
   );
 }
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5M82LHTXN9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-5M82LHTXN9');
-</script>
 
 export default App;
