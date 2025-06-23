94049beff5497f7b7eff70c5412939b61afcc460
Symmetric-Web <symmetricwebsite@gmail.com>
2025-06-23 13:39:40 +0530

    bug fix 2

--- src/index.js
+++ src/index.js
@@ -10,15 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5M82LHTXN9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-5M82LHTXN9');
-</script>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
